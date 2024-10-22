3c5229eca6bffe8f0019403d8db72ab9e2b33c2e
Inugurthi Raghunadh <107241095+inugurthiraghunadh@users.noreply.github.com>
2024-10-22 09:52:47 +0530

    student_data
    added student data

---
+++ Models/Sttduent.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAapp.Models
+{
+    public class Sttduent
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public decimal price { get; set; }  
+
+      
+    }
+}
